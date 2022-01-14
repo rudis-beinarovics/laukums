dee10a5a5b92d233919c227fa4852816d9663daf
rudis-beinarovics <97430467+rudis-beinarovics@users.noreply.github.com>
2022-01-14 14:43:42 +0200

    sfsf

--- script.js
+++ script.js
@@ -15,6 +15,23 @@ function nolasa() {
     return false;
   }
 }
+
+function vaiir(){
+const m = nolasa();
+m1=m.m1;m2=m.m2; m3=m.m3; 
+let mm= Math.max(m1,m2,m3);
+let p2= m1+m2+m3;
+let pm= p2-mm;
+//console.log(pm);
+//console.log(mm);
+if (mm < pm) {
+  return {m1,m2,m3};
+} else {x``
+  console.log(false);
+  return false;
+}
+}
+
 function per(m1,m2,m3){
     const m = nolasa();
     m1=m.m1;m2=m.m2; m3=m.m3;  
@@ -23,14 +40,40 @@ function per(m1,m2,m3){
   console.log(p)
   return p;
 }
+
 function lauk(m1,m2,m3) {
-  var s =per(m1,m2,m3);
   const m = nolasa();
   const p = per()
+  var s = p/2;
   m1=m.m1;m2=m.m2; m3=m.m3;
 
   var area = Math.sqrt(s*(s-m1)*(s-m2)*(s-m3))
+  //console.log()
   console.log(area);
   return area;
-  
+
+
+
+}
+function druka(){
+  const area=lauk()
+  const p=per()
+let rindas = document.querySelector('.rindas'); 
+ if(area>0 && p>0){
+rindas.innerHTML += `
+<tr>
+    <td>${p}</td>
+    <td>${area}</td>
+</tr>`
+} 
+ else {
+  let rindas = document.querySelector('.rindas'); 
+  let area = "";
+  let p = "Trijsturis neeksiste"
+  rindas.innerHTML += `
+<tr>
+    <td>${p}</td>
+    <td>${area}</td>
+</tr>`
+}
 }
