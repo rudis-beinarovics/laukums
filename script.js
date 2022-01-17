2f054c4b6ed1bd0737d9407c7bee3ae336edcd03
rudis-beinarovics <97430467+rudis-beinarovics@users.noreply.github.com>
2022-01-17 09:37:20 +0200

    sdadas

--- script.js
+++ script.js
@@ -8,46 +8,79 @@ function nolasa() {
   const m2 = parseFloat(mala2.value);
   const m3 = parseFloat(mala3.value);
   if (m1 > 0 && m2 > 0 && m3 > 0) {
-    console.log({m1,m2,m3});
-    return {m1,m2,m3};
+    console.log({ m1, m2, m3 });
+    return { m1, m2, m3 };
   } else {
     console.log(false);
     return false;
   }
 }
 
-function vaiir(){
-const m = nolasa();
-m1=m.m1;m2=m.m2; m3=m.m3; 
-let mm= Math.max(m1,m2,m3);
-let p2= m1+m2+m3;
-let pm= p2-mm;
-//console.log(pm);
-//console.log(mm);
-if (mm < pm) {
-  return {m1,m2,m3};
-} else {x``
-  console.log(false);
-  return false;
+function vaiir() {
+  const m = nolasa();
+  m1 = m.m1; m2 = m.m2; m3 = m.m3;
+  let mm = Math.max(m1, m2, m3);
+  let p2 = m1 + m2 + m3;
+  let pm = p2 - mm;
+  //console.log(pm);
+  //console.log(mm);
+  if (mm < pm) {
+    return { m1, m2, m3 };
+  } else {
+    ``
+    console.log(false);
+    return false;
+  }
 }
+function trijveids() {
+  const mm = vaiir();
+  const m = nolasa();
+  m1 = m.m1; m2 = m.m2; m3 = m.m3;
+  if (m1 = m2 || (m1 = m3) || (m2 = m3)) {
+    string = "Vienadsanu trijsturis";
+  }else{
+      if (m1 = m2 = m3) {
+    string = "Vienadmalu trijsturis";
+      
+  }
+ else {
+    string = "Vienkars trijsturis";
+    
+  }
 }
 
-function per(m1,m2,m3){
-    const m = nolasa();
-    m1=m.m1;m2=m.m2; m3=m.m3;  
-    console.log(m1,m2,m3);
-  const p=m1+m2+m3;
+
+ if(string="Vienadsanu trijsturis"){
+ v= "Vienadsanu trijsturis"
+ }else{
+   if(stringstring = "Vienadmalu trijsturis"){
+   }else {
+v="Vienkars trijsturis"
+
+}
+}
+console.log(v);
+}
+
+
+
+
+function per(m1, m2, m3) {
+  const m = nolasa();
+  m1 = m.m1; m2 = m.m2; m3 = m.m3;
+  console.log(m1, m2, m3);
+  const p = m1 + m2 + m3;
   console.log(p)
   return p;
 }
 
-function lauk(m1,m2,m3) {
+function lauk(m1, m2, m3) {
   const m = nolasa();
   const p = per()
-  var s = p/2;
-  m1=m.m1;m2=m.m2; m3=m.m3;
+  var s = p / 2;
+  m1 = m.m1; m2 = m.m2; m3 = m.m3;
 
-  var area = Math.sqrt(s*(s-m1)*(s-m2)*(s-m3))
+  var area = Math.sqrt(s * (s - m1) * (s - m2) * (s - m3))
   //console.log()
   console.log(area);
   return area;
@@ -55,25 +88,33 @@ function lauk(m1,m2,m3) {
 
 
 }
-function druka(){
-  const area=lauk()
-  const p=per()
-let rindas = document.querySelector('.rindas'); 
- if(area>0 && p>0){
-rindas.innerHTML += `
+function druka() {
+  const area = lauk()
+  const p = per()
+  const v = trijveids()
+
+  let rindas = document.querySelector('.rindas');
+  if (area > 0 && p > 0) {
+    rindas.innerHTML += `
 <tr>
+<td>${v}</td>
     <td>${p}</td>
     <td>${area}</td>
 </tr>`
-} 
- else {
-  let rindas = document.querySelector('.rindas'); 
-  let area = "";
-  let p = "Trijsturis neeksiste"
-  rindas.innerHTML += `
+  }
+  else {
+    let rindas = document.querySelector('.rindas');
+    let area = "";
+    let p = "Trijsturis neeksiste"
+  }
+    rindas.innerHTML += `
 <tr>
+<td>${v}</td>
     <td>${p}</td>
     <td>${area}</td>
 </tr>`
-}
-}
+  }
+
+
+
+
---
+++ test.js
@@ -0,0 +1,16 @@
+
+var i = 10;
+
+if (i == 10) {
+
+if (i < 15)
+	document.write("i is smaller than 15");
+
+// Nested - if statement
+// Will only be executed if statement above
+// it is true
+if (i < 12)
+	document.write("i is smaller than 12 too");
+else
+	document.write("i is greater than 15");
+}
